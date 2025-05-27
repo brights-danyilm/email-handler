6193ccba3d548302223d3a54a1361a6444da0459
danyil <danyil.mahalias@brights.io>
2025-05-27 15:44:55 +0300

    upd: script explained

--- sh/save_mail.sh
+++ sh/save_mail.sh
@@ -1,1 +1,10 @@
+# 1. `cat |` -> puts given email file to stdin
+# 2. `env $(cat /opt/email-handler/.env | xargs)` -> parses .env file and appends 
+# to node call
+# 3. `node /opt/email-handler/dist/index.js` -> call script
+# 4. `"$1"` -> Postfix is configured to pass `sender` as first (and only)
+# argument to this script, script passes it to node (`process.argv[2]`)
+#
+# `/opt/email-handler/dist/index.js` -> where the node.js project is located
+# on the server
 cat | env $(cat /opt/email-handler/.env | xargs) node /opt/email-handler/dist/index.js "$1"
